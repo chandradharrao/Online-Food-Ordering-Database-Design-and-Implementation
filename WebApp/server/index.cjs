6bae2985f0f0546596e9b04165ad6473b1813f66
chandradharrao <chandradhar.rao@gmail.com>
2021-11-07 00:48:46 +0530

    Set up  Express backend

--- WebApp/server/index.cjs
+++ WebApp/server/index.cjs
@@ -1,11 +1,35 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+app.use(express.json()); //parse incoming req to json
+const pool = require("./db");
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/",(req,res)=>{
-    res.send("Hello!!")
-})
+const insertInto = (tableName,columnNames,values)=>{
+    /*pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+    if (error) {
+      throw error
+    } */
+    const query=`INSERT INTO ${tableName} (${columnNames}) VALUES (${values});`;
+    pool.query(query,(err,results)=>{
+        if(err) throw err;
+        else return results;
+    })
+}
+
+app.post("/login",(req,res)=>{
+    console.log(req.body);
+    const isAdmin = req.body['isAdmin'];
+    if(!isAdmin){
+        const email = req.body["email"];
+        const password=req.body["password"];
+        if(!email || !password){
+            res.status(422).json({error:"Please fil all fields"});
+        }else{
+            res.status(200).json({success:"Recieved all details"});
+        }
+    }
+}) 
 
 app.listen(PORT,()=>{
     console.log(`Running on PORT ${PORT}....`)
